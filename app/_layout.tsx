dbcca5cfdca4d912adbcddd741ceea7bffc850d7
willyyypatootieee <blueplayz26@gmail.com>
2026-04-20 16:25:03 +0700

    feat: update RootLayout to replace 'index' and 'login' screens with 'auth' and '(tabs)'

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,10 +5,9 @@ export default function RootLayout() {
   return (
     <>
       <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
         <Stack.Screen name="onboarding" />
-        <Stack.Screen name="login" />
-        <Stack.Screen name="home" />
+        <Stack.Screen name="auth" />
+        <Stack.Screen name="(tabs)" />
       </Stack>
       <StatusBar style="auto" />
     </>
